013ee1ec856ce62cdac168e5a09401a74ff910af
‘2458578637’ <2458578637@qq.com>
2023-03-28 18:14:09 +0800

    1

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,7 @@
 export async function onRequestPost(context) {
   const { request } = context
   try {
-    const file = await request.formData()
+    const file = await request.body
     const t = file.get('file')
     const buffer = await t.arrayBuffer()
     const content = await arrayBufferToBase64(buffer)
