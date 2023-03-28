7fc58eef43a8f39b677c588bfad1dcc012574713
‘2458578637’ <2458578637@qq.com>
2023-03-28 18:10:50 +0800

    1

--- functions/test.js
+++ functions/test.js
@@ -3,8 +3,19 @@ export async function onRequestPost(context) {
   try {
     const file = await request.formData()
     const t = file.get('file')
-    return new Response('输出' + t.size)
+    const buffer = f.arrayBuffer()
+    return new Response('输出' + buffer)
   } catch (error) {
     return new Response(error)
   }
+}
+
+async function arrayBufferToBase64(buffer) {
+  let binary = ''
+  const bytes = new Uint8Array(buffer)
+  const len = bytes.byteLength
+  for (let i = 0; i < len; i += 1) {
+    binary += String.fromCharCode(bytes[i])
+  }
+  return btoa(binary)
 }
