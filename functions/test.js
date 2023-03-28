adf12dd97191c460305d560b2f55cf4321a7adec
‘2458578637’ <2458578637@qq.com>
2023-03-28 20:32:05 +0800

    1

--- functions/test.js
+++ functions/test.js
@@ -71,23 +71,22 @@ export async function onRequestPost(context) {
       body: formData
     })
 
-    return res2
-
-    //成功
-    // if (res.status == 201) {
-    //   outBody = JSON.stringify({
-    //     code: 200,
-    //     msg: '请求成功',
-    //     data: {
-    //       '7ED': `https://raw.githubusercontents.com/${or}/master/${pathname}/${uuid}${ext}`,
-    //       JsDelivr: `https://testingcf.jsdelivr.net/gh/${or}@master/${pathname}/${uuid}${ext}`
-    //     }
-    //   })
-    //   outStatus = 200
-    // } else {
-    //   outBody = res.body
-    //   outStatus = res.status
-    // }
+    成功
+    if (res1.status == 201) {
+      outBody = JSON.stringify({
+        code: 200,
+        msg: '请求成功',
+        data: {
+          '7ED': `https://raw.githubusercontents.com/${or}/master/${pathname}/${uuid}${ext}`,
+          JsDelivr: `https://testingcf.jsdelivr.net/gh/${or}@master/${pathname}/${uuid}${ext}`,
+          telegra: res2[0]?.src || res2[0].error
+        }
+      })
+      outStatus = 200
+    } else {
+      outBody = res1.body
+      outStatus = res1.status
+    }
   } catch (err) {
     outBody = JSON.stringify(err.stack) || err
     outStatus = 500
